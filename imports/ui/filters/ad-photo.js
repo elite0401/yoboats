b8d5f96a9a6c2528b110f0536482d4bcefd3b2cf
Dan <dan_steen@zoho.com>
2017-05-17 16:19:47 +0800

    commented

--- imports/ui/filters/ad-photo.js
+++ imports/ui/filters/ad-photo.js
@@ -10,11 +10,9 @@ angular.module(name, [])
 		if(!photo) {
 			photo = 0;
 		}
-
 		if(!ad.photos || ad.photos.length < 1){
 			return '/noPhoto.png';
 		}
-
 		return Meteor.settings.public.awsUrl + ad._id + '/' + ad.photos[photo];
 	};
 });
